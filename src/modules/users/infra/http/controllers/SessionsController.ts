c0a41c46144e48eb1cfc7a47bfa30c3f4942e7a8
Italo Barboza <italo_barboza@hotmail.com>
2021-01-17 01:24:51 -0300

    correcao do caminho

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -8,17 +8,7 @@ import AuthenticateUserSerivce from "@modules/users/services/AuthenticateUserSer
 export default class SessionsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { email, password } = request.body;
-/*
-    const secretKey = "marcosborges"
 
-    const Crypto = new crypto(secretKey);
-
-    const plainText = "Marcos Borges";
-    const cipherText = Crypto.encrypt(plainText);
-    const decipherText = Crypto.decrypt(cipherText);
-    console.log("Decipher Text : " + decipherText);
-    console.log("Crypt Text : " + cipherText);
-*/
     const authenticateUser = container.resolve(AuthenticateUserSerivce);
 
     const { user, token } = await authenticateUser.execute({
